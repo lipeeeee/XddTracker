5e95f08367eafac5c689f757f11494745a9db5b2
lipeeeee <filipe.pereirafga@gmail.com>
2025-01-10 19:33:10 +0000

    install.py

---
+++ XddTracker/commands.lua
@@ -0,0 +1,13 @@
+-- Slash Command to show death counts
+SLASH_XDDTRACKER1 = "/deaths"
+SlashCmdList["XDDTRACKER"] = function()
+    print("XddTracker Death List:")
+    for name, count in pairs(XddTracker.DB) do
+        print(name .. " has died " .. count .. " times.")
+    end
+end
+
+SLASH_XDDTRACKER2 = "/xdd"
+SlashCmdList["XDDTRACKER2"] = function()
+end
+
--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 import os
 
+zip_str = ""
+local_addon_path = "XddTracker/"
 wow_path = "/mnt/d/Games/World\ of\ Warcraft\ 3.3.5a/"
 src_folder = "src"
 addon_path = wow_path + "Interface/AddOns/XddTracker/"
@@ -12,7 +14,18 @@ files_to_install = [
     f"{src_folder}/ui.lua"
 ]
 
+# import directly
 os.system(f"mkdir -p {addon_path}")
 for file in files_to_install:
   os.system(f"cp {file} {addon_path}")
-  print(f"installed {file}")
+  print(f"> installed {file}")
+  zip_str = zip_str + f" {file}"
+
+# zip
+zip_str = zip_str[1:]
+os.system(f"mkdir -p {local_addon_path}")
+os.system(f"cp {src_folder}/* {local_addon_path}")
+os.system(f"cp XddTracker.toc {local_addon_path}")
+os.system(f"zip XddTracker.zip {local_addon_path} -r")
+print(f"> compiled addon into {local_addon_path}")
+
